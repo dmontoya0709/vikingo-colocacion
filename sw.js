66730dd7f16d3c6c1cc6dda8720ee3a227710058
dmontoya0709 <david.montoya@grupopdc.com>
2026-09-07 10:08:37 -0600

    Update cache version and asset list in sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "vikingo-colocacion-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+const CACHE = "vikingo-colocacion-v2";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./Favicon%20verde.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
