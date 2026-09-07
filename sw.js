19d7ea2aecc30a1681a225e9349419a934bfe311
dmontoya0709 <david.montoya@grupopdc.com>
2026-09-07 16:00:35 -0600

    Update cache version from v4 to v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vikingo-colocacion-v4";
+const CACHE = "vikingo-colocacion-v5";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./Favicon%20verde.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
